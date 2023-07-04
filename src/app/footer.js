f6baf6f67a6be7f8c2fd6e3f9776a7a5a01acfee
Ibrahim Yemi S <sharafadeen.might@gmail.com>
2023-07-04 12:20:00 +0100

    life push

---
+++ src/app/footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="p-4 bg-gray-900 md:p-8 lg:p-10">
+      <div className="mx-auto max-w-screen-xl text-center">
+        <hr/>
+        <p className="text-base md:text-xl my-2">BEATO CONSULTING</p>
+        <p className="my-3 text-gray-100">Business Center 1, M Floor, The Meydan Hotel, Nad Al Sheba, Dubai, U.A.E</p>
+        <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-300">
+          <li>
+            <a
+              href="https://alstrada.com"
+              className="mr-4 hover:underline md:mr-6 "
+            >
+              About Alstrada
+            </a>
+          </li>
+          <li>
+            <a href="https://alstrada.com" className="mr-4 hover:underline md:mr-6">
+              Have a question?
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:info@beatoconsultingllc.com"
+              className="mr-4 hover:underline md:mr-6"
+            >
+              Email us: info@beatoconsultingllc.com
+            </a>
+          </li>
+          <li>
+            <a href="tel:+971549962845" className="mr-4 hover:underline md:mr-6">
+              Call us on +971 54 996 2845
+            </a>
+          </li>
+          <li>
+            <a href="tel:+971509962804" className="mr-4 hover:underline md:mr-6">
+              Or call +971 50 996 2804
+            </a>
+          </li>
+        </ul>
+        <span className="text-sm text-gray-100 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="/" className="hover:underline">
+            Beato Consulting™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Beato Consultingllc',
+  description: 'unique consulting services for data strategy and technologies',
+  icons: {
+    icon: '/logo.png',
+  }
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
